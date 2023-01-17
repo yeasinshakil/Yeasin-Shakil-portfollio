93dd7faf311c9545fbe29443ea84db08cef2cd20
Yeasin_Shakil <shakilrock1998@gmail.com>
2023-01-17 20:32:41 +0600

    projects

--- src/components/portfolio/Works.jsx
+++ src/components/portfolio/Works.jsx
@@ -1,9 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { projectData } from '../portfolio/Data';
 import { projectNav } from '../portfolio/Data';
 import WorkItems from './WorkItems';
 
 const Works = () => {
+    const [item, setItem] = useState({name: 'all'});
+    const [projects, setProjects] = useState([]);
+    const [active, setActive] = useState(0);
+
+    useEffect(()=> {
+        if(item.name === 'all') {
+            setProjects(projectData);
+        }
+        else {
+            var newProjects = projectData.filter((project)=> {
+                return project.catagory === item.name;
+            });
+        }
+        setProjects(newProjects);
+    }, [item])
+
+
     return (
         <div>
             <div className="work_filters">
