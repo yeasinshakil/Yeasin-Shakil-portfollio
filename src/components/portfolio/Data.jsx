d0d08e6b32804b473f7c563e36e9785dd103999e
yeasinshakil <shakilrock1998@gmail.com>
2024-02-13 11:47:03 +0600

    fileshpere added

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -3,18 +3,20 @@ import dream from '../../assets/dream.jpg';
 import game from '../../assets/breakout game.jpg';
 import portfolio from '../../assets/portfolio.jpg';
 import moviflex from '../../assets/moviflex-cover.png'
-import DropBox from '../../assets/Dropbox Clone.png'
+import FileSphere from '../../assets/FileSphere.png'
 import ChatGPT from '../../assets/ChatGPT.png'
 
 export const projectData = [
 
     {
         id: 1,
-        image: DropBox,
-        title: 'Dropbox Clone',
+        image: FileSphere,
+        title: 'FileSphere',
         catagory: "app",
-        demo: 'https://dropbox-clone-kappa.vercel.app/',
-        github: 'https://github.com/yeasinshakil/dropbox-clone',
+        demo: 'https://filesphere-kappa.vercel.app/',
+        github: 'https://github.com/yeasinshakil/fileSphere-Next-Typescript',
+        description: 'FileSphere is a cloud-based file storage and collaboration platform that people manage and share their digital content. FileSphere allows users to store files securely in the cloud and access them from any device, anywhere, anytime with an internet connection.',
+        tech: 'Next Js, Typescript, Zustand, ShadCn, Tailwind',
     },
     {
         id: 23578,
@@ -23,6 +25,8 @@ export const projectData = [
         catagory: "web",
         demo: 'https://chat-gpt-type-script-next-js.vercel.app/',
         github: 'https://github.com/yeasinshakil/chatGPT-TypeScript-NextJs',
+        description: '',
+        tech: 'Next Js, Typescript, ShadCn, Tailwind',
     },
     {
         id: 111,
@@ -31,6 +35,8 @@ export const projectData = [
         catagory: "web",
         demo: 'https://emmajohn-store.netlify.app/',
         github: 'https://github.com/yeasinshakil/eCommerce-website-Strapi',
+        description: '',
+        tech: '',
     },
     {
         id: 12,
@@ -39,6 +45,8 @@ export const projectData = [
         catagory: "web",
         demo: 'https://moviflex-by-shakil.netlify.app/',
         github: 'https://github.com/yeasinshakil/Moviflex-app',
+        description: '',
+        tech: '',
     },
 
 
@@ -49,6 +57,8 @@ export const projectData = [
         catagory: "design",
         demo: 'https://heartfelt-raindrop-9a4add.netlify.app',
         github: 'https://github.com/YeasinShakil/disney-plus-clone',
+        description: '',
+        tech: '',
     },
     {
         id: 4,
@@ -57,6 +67,8 @@ export const projectData = [
         catagory: "web",
         demo: ' https://yeasinshakil.github.io/BreakOut-2D-Game/',
         github: 'https://github.com/YeasinShakil/BreakOut-2D-Game',
+        description: '',
+        tech: '',
     },
     {
         id: 5,
@@ -65,6 +77,8 @@ export const projectData = [
         catagory: "design",
         demo: 'https://yeasin-shakil.netlify.app',
         github: 'https://github.com/YeasinShakil/Yeasin-Shakil-portfollio',
+        description: '',
+        tech: '',
     },
 
 ]
